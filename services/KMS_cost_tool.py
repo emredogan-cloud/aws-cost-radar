93754f4b3686af7c1cd43f92dafd23bcdf3fe18c
emredogan-cloud <ed897854@gmail.com>
2026-02-04 21:27:08 +0300

    The KMS service has been redesigned to include more in-depth business logic.

--- services/KMS_cost_tool.py
+++ services/KMS_cost_tool.py
@@ -4,14 +4,22 @@ from dataclasses import dataclass,field
 from prettytable import PrettyTable
 from botocore.exceptions import ClientError
 from concurrent.futures import ThreadPoolExecutor,as_completed
+from typing import Optional
 
 
 @dataclass
 class KMSFinding:
-    region : str
-    key_id : str
-    alias: str
-    key_arn: str
+    Region : str
+    Key_İd : str
+    Alias: str
+    Key_Arn: str
+    Key_State : str = 'UNKOWN'
+    Key_Usage: str = 'UNKOWN'
+    Key_Spec: str = 'UNKOWN'
+    Key_Manager: str = 'UNKOWN'
+    Origin: str = 'UNKOWN'
+    Rotation_Reason: str = 'N/A'
+    rotation_enabled: Optional[bool] = None
     
 
 class KMSCollector:
@@ -20,14 +28,21 @@ class KMSCollector:
         self.logger = get_logger('KMS_LİST' , 'INFO')
         self.ec2 = self.manager.get_client('ec2')
         self.kms = self.manager.get_client('kms')
-        self.table = PrettyTable(["Region", "KeyId","Alias","KeyArn"])
+        self.table = PrettyTable(["Region", "KeyId","Alias","KeyArn","Rotation","KeyState","KeyUsage","KeySpec","KeyManager","Origin","RotationReason"])
 
-
-        
     def get_regions(self):
         
         region = [r['RegionName'] for r in self.ec2.describe_regions()['Regions']]
         return region
+
+    def get_rotation_status(self, kms_client ,key_id) -> Optional[bool]:
+        try:
+            response = kms_client.get_key_rotation_status(KeyId=key_id)
+            return response['KeyRotationEnabled']
+        except ClientError as e:
+            error = e.response['Error']['Code']
+            self.logger.debug(f'Rotation status not available for {key_id}: {error}')
+            return None
 
 
     def kms_alias(self,kms_client):
@@ -43,6 +58,25 @@ class KMSCollector:
         return  alias_map
 
 
+    def decribe_key_meta(self, kms_client ,key_id : str) -> dict:
+        try:
+            response = kms_client.describe_key(KeyId = key_id)
+            meta = response['KeyMetadata']
+
+            return {
+            'key_state' : meta['KeyState'],
+            'key_usage' : meta['KeyUsage'],
+            'key_manager' : meta['KeyManager'],
+            'origin' : meta['Origin'],
+            'key_spec' : meta['KeySpec']
+            }
+        except ClientError:
+            return {
+                'KeyState':'ERROR'
+            }
+
+
+
     def list_keys(self,kms_client):
         keys_list = []
         for page in kms_client.get_paginator('list_keys').paginate():
@@ -56,18 +90,21 @@ class KMSCollector:
         rows = []
         alias_map = self.kms_alias(kms)
         keys = self.list_keys(kms)
+        
 
         for key in keys:
             key_id = key['KeyId']
             key_arn = key['KeyArn']
             alias = alias_map.get(key_id , 'No Alias')
+            rotation = self.get_rotation_status(kms , key_id)
             rows.append(KMSFinding(
-                region=reg,
-                key_id=key_id,
-                alias=alias,
-                key_arn=key_arn
+                Region=reg,
+                Key_İd=key_id,
+                Alias=alias,
+                Key_Arn=key_arn,
+                rotation_enabled=rotation
             ))
-            
+
         return rows
 
         
@@ -91,7 +128,8 @@ class KMSCollector:
                             finding.region,
                             finding.key_id,
                             finding.alias,
-                            finding.key_arn
+                            finding.key_arn,
+                            'ENABLED' if finding.rotation_enabled is True else 'DİSABLED' if finding.rotation_enabled is False else 'N/A'
                         ])
 
                         
